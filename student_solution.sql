7622f5ffab19730a353a68bc5f0947903f2bcf07
YASAR ARAFATH S <yasarsulthan11@gmail.com>
2026-09-03 09:49:02 +0530

    Create Marksheet table and insert sample data

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,18 @@
+CREATE TABLE Marksheet(
+RollNo INT,
+Name VARCHAR(20),
+Department VARCHAR(20),
+Marks INT
+);
 
+INSERT INTO Marksheet VALUES
+(1,'Arun','CSE',85),
+(2,'Divya','IT',78),
+(3,'Karthik','CSE',92),
+(4,'Nisha','ECE',67),
+(5,'Rahul','IT',88);
+
+SELECT *
+FROM Marksheet
+WHERE Marks > 80
+ORDER BY Marks DESC;
